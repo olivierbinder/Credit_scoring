6b057c8669db69a2a440da05d80022605267c502
Olivier <>
2026-06-16 16:47:55 +0200

    inference + api + streamlit

---
+++ scripts/generate_base_for_inference.py
@@ -0,0 +1,32 @@
+# scripts/build_reference.py
+import pandas as pd
+
+from credit_scoring.config import DF_PROC_PATH, DIR_DATA_PROCESSED
+
+FEATURES = [
+    "EXT_SOURCE_2",
+    "EXT_SOURCE_3",
+    "EXT_SOURCE_1",
+    "PAYMENT_RATE",
+    "INSTAL_DPD_MEAN",
+    "DAYS_EMPLOYED",
+    "INSTAL_AMT_PAYMENT_SUM",
+    "AMT_ANNUITY",
+    "POS_CNT_INSTALMENT_FUTURE_MEAN",
+    "CODE_GENDER",
+    "OWN_CAR_AGE",
+    "NAME_EDUCATION_TYPE",
+    "DAYS_BIRTH",
+    "PREV_CNT_PAYMENT_MEAN",
+    "CC_CNT_DRAWINGS_ATM_CURRENT_MEAN",
+    "ACTIVE_DAYS_CREDIT_MAX",
+    "AMT_GOODS_PRICE",
+    "PREV_DAYS_LAST_DUE_1ST_VERSION_MEAN",
+    "POS_SK_DPD_DEF_MEAN",
+    "CC_CNT_DRAWINGS_CURRENT_VAR",
+]
+
+
+df = pd.read_parquet(DF_PROC_PATH)
+ref = df[["SK_ID_CURR"] + FEATURES]
+ref.to_parquet(DIR_DATA_PROCESSED / "reference.parquet", index=False)
--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Lance l'API FastAPI en arrière-plan
-uv run --no-dev uvicorn credit_scoring.interfaces.api:app_simple --host 0.0.0.0 --port 8000 &
-
-# Lance Streamlit en premier plan (pour garder le conteneur vivant)
-uv run --no-dev streamlit run src/credit_scoring/interfaces/app_streamlit.py --server.port 7860
-
-
-uv run streamlit run src/credit_scoring/interfaces/app_streamlit.py
+uv run --no-project uvicorn credit_scoring.interfaces.api:app --host 0.0.0.0 --port 8000 &
+uv run --no-project streamlit run src/credit_scoring/interfaces/app_streamlit.py --server.port 7860
